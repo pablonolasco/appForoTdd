356085836d53470d019af1847e41ad69a16a39f9
pablonolasco <pablo.nolasco@live.com.mx>
2020-07-19 13:45:51 -0500

    personalizar-el-directorio-tests-de-una-aplicacion-de-laravel-5-3

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Illuminate\Foundation\Testing\DatabaseTransactions;
+use \Illuminate\Foundation\Testing\DatabaseMigrations;
 class FeatureTestCase extends TestCase
 {
     use DatabaseTransactions;
